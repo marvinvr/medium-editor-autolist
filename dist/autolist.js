f9780ea45e22790bd688f6c48a8fcc338779f454
neyaoz <mail@necatiozal.com>
2017-12-14 17:45:53 +0300

    A minor bug has been fixed.
    Previous expression matches with any “*” or “1.” in a paragraph. But
    now, it should be on

--- dist/autolist.js
+++ dist/autolist.js
@@ -16,13 +16,13 @@
     },
     onInput: function (evt) {
       var list_start = this.base.getSelectedParentElement().textContent;
-      if (/1\.\s/.test(list_start) && this.base.getExtensionByName('orderedlist')){
+      if (/^\s*1\.\s/.test(list_start) && this.base.getExtensionByName('orderedlist')){
         this.base.execAction('delete');
         this.base.execAction('delete');
         this.base.execAction('delete');
         this.base.execAction('insertorderedlist');
       }
-      else if (/\*\s/.test(list_start) && this.base.getExtensionByName('unorderedlist')){
+      else if (/^\s*\*\s/.test(list_start) && this.base.getExtensionByName('unorderedlist')){
         this.base.execAction('delete');
         this.base.execAction('delete');
         this.base.execAction('insertunorderedlist');
